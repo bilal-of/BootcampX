c352cbb3d0a6f0a248c6ed9c77e73527a1579768
Bilal <bfarah@ualberta.ca>
2022-12-15 03:00:01 +0000

    injection teachers

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2] || "JUL02";
+const values = [cohortName];
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+    FROM teachers
+      JOIN assistance_requests ON teachers.id = teacher_id
+      JOIN students ON students.id = student_id
+      JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name = $1
+    ORDER BY teacher;
+`;
+pool.query(queryString, values).then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}; ${row.teacher}`);
+  });
+});
